cab76ff25bb4d1f7721992d54f63345e61cb7147
groovelb <groovelb@gmail.com>
2018-11-02 15:15:10 +0900

    mobile document updated

---
+++ mobile_ver2_bootstrap/js/menu_slide.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	var menu_on = false;
+	$("#menu").on("click",function(){
+		menu_on = (!menu_on);
+		$(".main_menu").toggleClass("main_menu_on");
+		$(this).toggleClass('open');
+		if(menu_on){
+			$("#logo").attr("src","img/logo_ver3.svg");
+			$(".header").addClass("header_fixed");
+		}
+		else{
+			$("#logo").attr("src","img/logo_ver3_white.svg");
+			$(".header").removeClass("header_fixed");
+		}
+		
+	});
+
+	/*이벤트 슬라이드*/
+	$("#event_slide").delay(500).animate({left:"-100%"},{duration:100});
+	$(".event_left").on("click",function(){
+		$("#event_slide").animate({left: "+=100%"},{duration:100});
+	});
+	$(".event_right").on("click",function(){
+		$("#event_slide").animate({left: "+=-100%"},{duration:100});
+	});
+});
