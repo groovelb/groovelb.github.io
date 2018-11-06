a775c4af83bab5eca1d7d219887810d36fcf59bc
groovelb <groovelb@gmail.com>
2018-11-06 15:29:43 +0900

    11/06 수정사항 반영

--- ver3_bootstrap/js/map.js
+++ ver3_bootstrap/js/map.js
@@ -37,8 +37,6 @@ function initMap() {
     				{lat: 1.3401, lng: 103.7189},
     				{lat: 1.3023, lng: 103.6089},
     				{lat: 1.3129, lng: 103.5389},
-
-
     			 ];
     var my_places = [
                     {lat: 1.4005, lng: 103.7914},
@@ -49,6 +47,9 @@ function initMap() {
                     {lat: 1.4007, lng: 102.8043},
                     {lat: 1.3619, lng: 103.7489},
                     {lat: 1.2957, lng: 103.8168}
+    ];
+     var my_hotel = [
+                    {lat: 1.3673, lng: 103.8662}
     ];
 
     // Create a map object and specify the DOM element
@@ -64,7 +65,8 @@ function initMap() {
     	var marker = new google.maps.Marker({
 	      map: map,
 	      position: place,
-	      title: 'Place Title'
+	      title: 'Place Title',
+          icon: 'img/hot_spot.svg'
 	    });
 
 	    marker.addListener('click', function(){
@@ -81,7 +83,32 @@ function initMap() {
           map: map,
           position: place,
           title: 'Place Title',
-          icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+          icon: 'img/my_place.svg'
+        });
+
+        marker.addListener('click', function(){
+            console.log("marker" + i + " click!");
+            document.getElementById("place_info_tab").classList.add("slide_on");
+        });
+    });
+    hide_markers();
+
+    // Create a marker and set its position.
+    my_hotel.forEach(function(place, i){
+        var marker_hotel = {
+            url: 'img/marker_hotel2.svg',
+            // This marker is 20 pixels wide by 32 pixels high.
+            size: new google.maps.Size(45, 63.5),
+            // The origin for this image is (0, 0).
+            origin: new google.maps.Point(0, 0),
+            // The anchor for this image is the base of the flagpole at (0, 32).
+            anchor: new google.maps.Point(0, 32)
+          };
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place,
+          title: 'Place Title',
+          icon: marker_hotel
         });
 
         marker.addListener('click', function(){
--- ver3_bootstrap/js/map2.js
+++ ver3_bootstrap/js/map2.js
@@ -5,10 +5,8 @@ function initMap() {
   	var myLatLng = {lat: 1.3534, lng: 103.8164};
     var places = [	
     				/*Left top*/
-    				
     				{lat: 1.4407, lng: 103.8259},
     				/*left bottom*/
-    			
     				{lat: 1.3473, lng: 103.8562},
     				/*right top*/
     				{lat: 1.4222, lng: 103.8686},
@@ -16,10 +14,22 @@ function initMap() {
     				/*right bottom*/
     				{lat: 1.3005, lng: 103.6914},
     				{lat: 1.3119, lng: 103.7089}
-
     			 ];
+
     var my_places = [
+                    {lat: 1.4005, lng: 103.7914},
+                    {lat: 1.3419, lng: 103.6889},
+                    {lat: 1.3673, lng: 103.8662},
+                    {lat: 1.4007, lng: 102.8043},
+                    {lat: 1.3619, lng: 103.7489},
+                    {lat: 1.2957, lng: 103.8168}
+    ];
+    var my_hotel = [
                     {lat: 1.3673, lng: 103.8662}
+    ];
+
+    var new_place = [
+
     ];
 
     // Create a map object and specify the DOM element
@@ -35,7 +45,8 @@ function initMap() {
     	var marker = new google.maps.Marker({
 	      map: map,
 	      position: place,
-	      title: 'Place Title'
+	      title: 'Place Title',
+          icon: 'img/hot_spot.svg'
 	    });
 
 	    marker.addListener('click', function(){
@@ -47,11 +58,11 @@ function initMap() {
     });
 
     // Create a marker and set its position.
-    my_places.forEach(function(place, i){
+    my_hotel.forEach(function(place, i){
         var marker_hotel = {
             url: 'img/marker_hotel2.svg',
             // This marker is 20 pixels wide by 32 pixels high.
-            size: new google.maps.Size(30, 43),
+            size: new google.maps.Size(45, 63.5),
             // The origin for this image is (0, 0).
             origin: new google.maps.Point(0, 0),
             // The anchor for this image is the base of the flagpole at (0, 32).
@@ -66,7 +77,25 @@ function initMap() {
 
         marker.addListener('click', function(){
             console.log("marker" + i + " click!");
+            document.getElementById("hotel_info_tab").classList.add("slide_on");
+            document.getElementById("place_info_tab").classList.remove("slide_on");
+        });
+    });
+
+    // Create a marker and set its position.
+    my_places.forEach(function(place, i){
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place,
+          title: 'Place Title',
+          icon: 'img/my_place.svg'
+        });
+
+        marker.addListener('click', function(){
+            console.log("marker" + i + " click!");
             document.getElementById("place_info_tab").classList.add("slide_on");
+            document.getElementById("hotel_info_tab").classList.remove("slide_on");
+            console.log(place);
         });
     });
   }
