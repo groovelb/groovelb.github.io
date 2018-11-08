93891f9607f4e9b44d7b1c983b8deb4b7aeac152
groovelb <groovelb@gmail.com>
2018-11-08 15:21:56 +0900

    업데이트
    3-1. 핫스팟& 동행 UX변경사항 반영
    3-2. 동행 신청받기 화면
    3-3. 문서 폰트 사이즈 규격 최종 확인
    3-4. 회의 때 작동안되었던 팝업 + 모바일 나머지 팝업 확인

---
+++ mobile_ver2_bootstrap/js/preference.js
@@ -0,0 +1,72 @@
+var preference = [
+ {
+ "keyword":"가족여행",
+ "check": 0
+ },
+ {
+ "keyword":"우정여행",
+ "check": 0
+ },
+ {
+ "keyword":"혼자여행",
+ "check": 0
+ },
+ {
+ "keyword":"커플여행",
+ "check": 0
+ },
+ {
+ "keyword":"배낭여행",
+ "check": 0
+ },
+ {
+ "keyword":"출장",
+ "check": 0
+ },
+ {
+ "keyword":"식도락",
+ "check": 0
+ },
+ {
+ "keyword":"문화탐방",
+ "check": 0
+ },
+ {
+ "keyword":"어드벤처",
+ "check": 0
+ },
+ {
+ "keyword":"부지런함",
+ "check": 0
+ },
+ {
+ "keyword":"여유로움",
+ "check": 0
+ },
+ {
+ "keyword":"럭셔리",
+ "check": 0
+ },
+ {
+ "keyword":"합리적인",
+ "check": 0
+ },
+ {
+ "keyword":"호텔",
+ "check": 0
+ },
+ {
+ "keyword":"호스텔",
+ "check": 0
+ },
+ {
+ "keyword":"아파트",
+ "check": 0
+ },
+ {"keyword":"리조트",
+  "check":0
+},
+{"keyword":"게스트하우스",
+  "check":0
+ }
+ ]
